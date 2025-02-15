e74958483e32aacfadc07fa560c26ca9a145357a
prath <prath@Prathyusha>
2025-02-15 16:56:55 +0530

    Prathyusha second commit, added MainClassPrathyu

---
+++ src/main/java/githubpack/GithubProject/MainClassPrathyu.java
@@ -0,0 +1,10 @@
+package githubpack.GithubProject;
+
+public class MainClassPrathyu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Checking merge conflicts");
+	}
+
+}
