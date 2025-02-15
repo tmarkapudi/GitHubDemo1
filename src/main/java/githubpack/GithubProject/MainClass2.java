a3ffe3d7c18f86d6704d958a072aa001a12661fd
prath <prath@Prathyusha>
2025-02-15 16:07:12 +0530

    Prathyusha first commit

---
+++ src/main/java/githubpack/GithubProject/MainClass2.java
@@ -0,0 +1,10 @@
+package githubpack.GithubProject;
+
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Main class 2");
+	}
+
+}
